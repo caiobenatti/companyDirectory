9d225b768d9bed5adc742b7d5488d3a75519690c
caio benatti <caio.benatti@gmail.com>
2020-11-25 01:31:06 +0000

    waiting for approval

--- libs/php/autenticateLogin.php
+++ libs/php/autenticateLogin.php
@@ -1,5 +1,9 @@
 <?php
 
+	// example use from browser
+	// http://localhost/companydirectory/libs/php/getAll.php
+
+	// remove next two lines for production
 	
 	ini_set('display_errors', 'On');
 	error_reporting(E_ALL);
@@ -25,61 +29,53 @@
 		exit;
 
 	}	
-	// $username = $_POST['userLogIn'];
-	// $password = $_POST['passwordLogIn'];
-// if (isset($_POST['submit'])) {
-    $query = 'SELECT * FROM users WHERE login=' . $_POST['userLogIn'] .' and password=' . $_POST['passwordLogIn'];
-    
+
+ $query = "SELECT login, password FROM users";
+
+//   WHERE login=' . $_POST['userLogIn'] .' and password=' . $_POST['passwordLogIn']
 	$result = $conn->query($query);
+	
+	if (!$result) {
+
+		$output['status']['code'] = "400";
+		$output['status']['name'] = "executed";
+		$output['status']['description'] = "query failed";	
+		$output['data'] = [];
+
+		mysqli_close($conn);
+
+		echo json_encode($output); 
+
+		exit;
+
+	}
+   	$data = [];
+
+	while ($row = mysqli_fetch_assoc($result)) {
+
+		array_push($data, $row);
+
+	}
+		   
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = $data;
+
+	
+	if($output['data'][0]['login'] == $_POST['userLogIn'] and $output['data'][0]['password'] == $_POST['passwordLogIn']){
+		echo header('Location:../../app.html');
+	}else{
+			echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+	echo header('Location:../../index.html');
+	echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+}
+
+    header('Content-Type: application/json; charset=UTF-8');
     
-    $count = $result->num_rows;
-
-    // if ($count == 1){
-    // $output['status']['code'] = "200";
-	// $output['status']['name'] = "ok";
-	// $output['status']['description'] = "success";
-	// $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	// echo '<script>window.location.replace("index.html")</script>';
-    // } else{
-    //     $output['status']['code'] = "400";
-	// 	$output['status']['name'] = "executed";
-	// 	$output['status']['description'] = "query failed";	
-	// 	$output['data'] = [];
-
-	// 	mysqli_close($conn);
-
-	// echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
-
-	// 	exit;
-
-    // }
-
-
-	// if (!$result) {
-
-	// 	$output['status']['code'] = "400";
-	// 	$output['status']['name'] = "executed";
-	// 	$output['status']['description'] = "query failed";	
-	// 	$output['data'] = [];
-
-	// 	mysqli_close($conn);
-
-	// 	echo json_encode($output); 
-
-	// 	exit;
-
-	// }
-
-	// $output['status']['code'] = "200";
-	// $output['status']['name'] = "ok";
-	// $output['status']['description'] = "success";
-	// $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-
-    
-	header('Content-Type: application/json; charset=UTF-8');
-    
-echo $count;
 	mysqli_close($conn);
 
-?>
 
+?>
